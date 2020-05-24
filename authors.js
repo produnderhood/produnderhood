7da2f60e0b74ec13a74cf8e9eff2ade74724403e
Agapov Alexey <agapov.one@gmail.com>
2020-05-24 21:48:23 +0500

    Add Svetlana Blazhnova
    #update

--- authors.js
+++ authors.js
@@ -6,6 +6,7 @@ export default authorId([
   //   first: '',  # id of first tweet
   //   post: false # Include only for author that hasn't finished his week yet. Will display link to twitter instead of archive
   // },
+  { username: 'produnderhood', first: '1262284882308411394' },
   { username: 'produnderhood', first: '1259761050548830209' },
   { username: 'Daiver9', first: '1257204668784095233' },
   { username: 'MojoIvan', first: '1254711513945968641' }
