13a4afde6c8dd54b9e4b48163af841ca849564e6
anybot <anybot@anyunderhood>
2020-11-15 22:55:29 +0500

    Update authors.js

--- authors.js
+++ authors.js
@@ -6,6 +6,7 @@ export default authorId([
   //   first: '',  # id of first tweet
   //   post: false # Include only for author that hasn't finished his week yet. Will display link to twitter instead of archive
   // },
+  { username: 'produnderhood', first: '1325712817438273542' },
   { username: 'produnderhood', first: '1323152530466050048' },
   { username: 'ow11et', first: '1320625915181830144' },
   { username: 'kidrulit', first: '1318165891175960576' },
